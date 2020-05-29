b496fcedc744af78a313cb131c289bc15f7d873f
Jane Greene <janegreenecasa@gmail.com>
2020-05-29 10:16:58 -0600

    completed story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bobby = Chef.create(name: "Bobby Flay")
+cat = Chef.create(name: "Cat Cora")
+
+tots = bobby.dishes.create(name:"Chili Lime Tots", description: "Spicy and delish")
+hot_pocket = bobby.dishes.create(name:"Hot Pocket 2.0", description: "Spicy and delish, flaming hot.")
+spring_roll = cat.dishes.create(name:"Spicy Spring Rolls", description: "Spicy and delish, great summer dish.")
+
+potato = Ingredient.create(name: "Potato shreds", calories: 80 )
+chili = Ingredient.create(name: "Chili powder", calories: 20 )
+lime = Ingredient.create(name: "Lime Juice", calories: 10 )
+pastry = Ingredient.create(name: "Puff Pastry", calories: 220 )
+wrap = Ingredient.create(name: "Spring Roll Wrapper", calories: 60 )
+
+DishIngredient.create(dish: tots, ingredient: potato)
+DishIngredient.create(dish: tots, ingredient: lime)
+DishIngredient.create(dish: tots, ingredient: chili)
+DishIngredient.create(dish: hot_pocket, ingredient: potato)
+DishIngredient.create(dish: hot_pocket, ingredient: pastry)
+DishIngredient.create(dish: hot_pocket, ingredient: chili)
+DishIngredient.create(dish: spring_roll, ingredient: wrap)
+DishIngredient.create(dish: spring_roll, ingredient: lime)
+DishIngredient.create(dish: spring_roll, ingredient: chili)
+DishIngredient.create(dish: spring_roll, ingredient: potato)
